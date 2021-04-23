00666f12944ebea59ae9dcfbffc80797d841c4b6
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2021-04-23 04:22:27 -0400

    Updated TM Learnsets for Water/Fire Starters

--- data/pokemon/base_stats/typhlosion.asm
+++ data/pokemon/base_stats/typhlosion.asm
@@ -19,5 +19,5 @@
 	;         hp, atk, def, spd, sat, sdf
 
 	; tmhm
-	tmhm ROCK_SMASH, FAKE_OUT, CUT, FALSE_SWIPE, FIRE_PUNCH, THUNDERPUNCH
+	tmhm ROCK_SMASH, FAKE_OUT, METAL_CLAW, CUT, FALSE_SWIPE, FIRE_PUNCH, THUNDERPUNCH
 	; end

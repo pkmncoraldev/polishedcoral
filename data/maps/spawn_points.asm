560e83ee24e9667bbaa7652115bf3b1cb048c9e4
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2021-04-30 11:01:48 -0400

    fixed spawn points

--- data/maps/spawn_points.asm
+++ data/maps/spawn_points.asm
@@ -6,14 +6,14 @@ ENDM
 
 SpawnPoints: ; 0x152ab
 	spawn PLAYER_HOUSE_2F,              3,  3
-	spawn SUNSET_BAY,                  25, 10
+	spawn SUNSET_BAY,                  25,  8
 	spawn DAYBREAK_VILLAGE,            13,  8
 	spawn GLINT_CITY,                  27, 16
 	spawn STARGLOW_VALLEY,			   17, 18
 	spawn LAKE_ONWA,				   17, 18
 	spawn SUNBEAM_ISLAND,			   17, 34
 	spawn EVENTIDE_VILLAGE,			   13, 18
-	spawn FLICKER_STATION,			   37, 28
+	spawn FLICKER_STATION,			   37, 30
 	spawn TWINKLE_TOWN,				   11, 48
 	spawn LUSTER_CITY_RESIDENTIAL,	   17, 26
 	

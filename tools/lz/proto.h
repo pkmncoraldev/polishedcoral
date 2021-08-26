6564043593157c7edb9f5c965b2bbcc0b6c2d438
Rangi <remy.oukaour+rangi42@gmail.com>
2021-08-25 22:06:46 -0400

    Use the up-to-date tools (fixed scan_includes and dynamic-programming lzcomp)

---
+++ tools/lz/proto.h
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#define MAX_FILE_SIZE            32768
+#define SHORT_COMMAND_COUNT         32
+#define MAX_COMMAND_COUNT         1024
+#define LOOKBACK_LIMIT             128 /* highest negative valid count for a copy command */
+
+#if __STDC_VERSION__ >= 201112L
+	// <noreturn.h> forces "noreturn void", which is silly and redundant; this is simpler
+	#define noreturn _Noreturn void
+#else
+	#define noreturn void /* fallback */
+#endif
+
+struct command {
+  unsigned command: 3; // commands 0-6 as per compression spec; command 7 is used as a dummy placeholder
+  unsigned count:  12; // always equals the uncompressed data length
+  signed value:    17; // offset for commands 0 (into source) and 4-6 (into decompressed output); repeated bytes for commands 1-2
+};
+
+struct options {
+  const char * input;
+  const char * output;
+  unsigned char mode; // 0: compress, 1: compress to text, 2: uncompress, 3: dump commands as text
+  unsigned char alignment; // 1 << value
+};
+
+// global.c
+extern const unsigned char bit_flipping_table[];
+extern char option_name_buffer[];
+
+// main.c
+int main(int, char **);
+struct command * compress(const unsigned char *, unsigned short *);
+
+// options.c
+struct options get_options(int, char **);
+unsigned parse_numeric_option_argument(char ***, unsigned);
+const char * get_argument_for_option(char ***, const char **);
+noreturn usage(const char *);
+
+// output.c
+void write_commands_to_textfile(const char *, const struct command *, unsigned, const unsigned char *, unsigned char);
+void write_commands_and_padding_to_textfile(const char *, const struct command *, unsigned, const unsigned char *, unsigned, unsigned);
+void write_command_to_textfile(FILE *, struct command, const unsigned char *);
+void write_commands_to_file(const char *, const struct command *, unsigned, const unsigned char *, unsigned char);
+void write_command_to_file(FILE *, struct command, const unsigned char *);
+void write_raw_data_to_file(const char *, const void *, unsigned);
+
+// uncomp.c
+struct command * get_commands_from_file(const unsigned char *, unsigned short * restrict, unsigned short * restrict);
+unsigned char * get_uncompressed_data(const struct command *, const unsigned char *, unsigned short *);
+
+// util.c
+noreturn error_exit(int, const char *, ...);
+unsigned char * read_file_into_buffer(const char *, unsigned short *);
+short command_size(struct command);
+unsigned short compressed_length(const struct command *, unsigned short);
+
+// dpcomp.c
+struct command * compress_dp(const unsigned char * data, const unsigned char * bitflipped, unsigned short * size);
---
+++ tools/lz/util.c
@@ -0,0 +1,35 @@
+#include "proto.h"
+
+noreturn error_exit (int error_code, const char * error, ...) {
+  va_list ap;
+  va_start(ap, error);
+  fputs("error: ", stderr);
+  vfprintf(stderr, error, ap);
+  va_end(ap);
+  fputc('\n', stderr);
+  exit(error_code);
+}
+
+unsigned char * read_file_into_buffer (const char * file, unsigned short * size) {
+  FILE * fp = file ? fopen(file, "rb") : stdin;
+  if (!fp) error_exit(1, "could not open file %s for reading", file);
+  unsigned char * buf = malloc(MAX_FILE_SIZE + 1);
+  int rv = fread(buf, 1, MAX_FILE_SIZE + 1, fp);
+  if (file) fclose(fp);
+  if (rv < 0) error_exit(1, "could not read from file %s", file);
+  if (rv > MAX_FILE_SIZE) error_exit(1, "file %s is too big", file ? file : "<standard input>");
+  *size = rv;
+  return buf;
+}
+
+short command_size (struct command command) {
+  short header_size = 1 + (command.count > SHORT_COMMAND_COUNT);
+  if (command.command & 4) return header_size + 1 + (command.value >= 0);
+  return header_size + command.command[(short []) {command.count, 1, 2, 0}];
+}
+
+unsigned short compressed_length (const struct command * commands, unsigned short count) {
+  unsigned short current, total = 0;
+  for (current = 0; current < count; current ++) if (commands[current].command != 7) total += command_size(commands[current]);
+  return total;
+}

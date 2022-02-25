6427f1b9138cfe50a2dd4bb66697872620a74e9c
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2022-02-25 04:12:12 -0500

    Fixed Minor Collision Issue

--- data/tilesets/spooky_collision.asm
+++ data/tilesets/spooky_collision.asm
@@ -24,7 +24,7 @@
 	tilecoll WALL, WALL, WALL, WALL ; 17
 	tilecoll WALL, WALL, WALL, WALL ; 18
 	tilecoll WALL, WALL, WALL, WALL ; 19
-	tilecoll WALL, WALL, WALL, DOOR ; 1a
+	tilecoll WALL, WALL, WALL, WALL ; 1a
 	tilecoll WALL, WALL, WALL, WALL ; 1b
 	tilecoll WALL, WALL, WALL, WALL ; 1c
 	tilecoll WALL, WALL, DOOR, DOOR ; 1d

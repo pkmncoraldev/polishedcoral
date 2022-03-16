4860c43d9a2fb62e0355710b0ae815bf5e13f625
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2022-03-15 20:41:23 -0400

    Added Surf Encounters

--- data/wild/north_onwa_water.asm
+++ data/wild/north_onwa_water.asm
@@ -1,5 +1,265 @@
 ; N. Onwa Pokémon in water
 
+	map_id SUNSET_BAY
+	db 6 percent ; encounter rate
+	db 29, MAREANIE		;30%
+	db 29, HORSEA		;30%
+	db 29, WINGULL		;20%
+	db 29, CORSOLA		;10%
+	db 29, STARYU		;5%
+	db 29, STARYU		;4%
+	db 29, STARYU		;1%
+
+	map_id SUNSET_CAPE
+	db 6 percent ; encounter rate
+	db 29, WINGULL		;30%
+	db 29, CHINCHOU		;30%
+	db 29, HORSEA		;20%
+	db 29, MAREANIE		;10%
+	db 29, MAREANIE		;5%
+	db 29, STARYU		;4%
+	db 29, STARYU		;1%
+	
+	map_id DAYBREAK_VILLAGE
+	db 15 percent ; encounter rate
+	db 29, DRATINI		;30%
+	db 29, DRATINI		;30%
+	db 29, DRATINI		;20%
+	db 29, DRATINI		;10%
+	db 29, DRATINI		;5%
+	db 29, DRATINI		;4%
+	db 29, DRATINI		;1%
+	
+	map_id ROUTE_2
+	db 6 percent ; encounter rate
+	db 29, POLIWHIRL	;30%
+	db 29, QUAGSIRE		;30%
+	db 29, FLOATZEL		;20%
+	db 29, POLIWHIRL	;10%
+	db 29, QUAGSIRE		;5%
+	db 29, FLOATZEL		;4%
+	db 29, FLOATZEL		;1%
+	
+	map_id DAYBREAK_GROTTO_1
+	db 8 percent ; encounter rate
+	db 29, SLOWPOKE		;30%
+	db 29, SLOWPOKE		;30%
+	db 29, QUAGSIRE		;20%
+	db 29, SLOWPOKE		;10%
+	db 29, QUAGSIRE		;5%
+	db 29, QUAGSIRE		;4%
+	db 29, QUAGSIRE		;1%
+	
+	map_id GLINT_GROVE
+	db 8 percent ; encounter rate
+	db 29, POLIWHIRL	;30%
+	db 29, POLIWHIRL	;30%
+	db 29, AZUMARILL	;20%
+	db 29, AZUMARILL	;10%
+	db 29, AZUMARILL	;5%
+	db 29, AZUMARILL	;4%
+	db 29, AZUMARILL	;1%
+
+	map_id ROUTE_3
+	db 15 percent ; encounter rate
+	db 29, DRATINI		;30%
+	db 29, DRATINI		;30%
+	db 29, DRATINI		;20%
+	db 29, DRATINI		;10%
+	db 29, DRATINI		;5%
+	db 29, DRATINI		;4%
+	db 29, DRATINI		;1%
+	
+	map_id STARGLOW_CAVERN_1F
+	db 6 percent ; encounter rate
+	db 29, QUAGSIRE		;30%
+	db 29, POLIWHIRL	;30%
+	db 29, AZUMARILL	;20%
+	db 29, QUAGSIRE		;10%
+	db 29, POLIWHIRL	;5%
+	db 29, AZUMARILL	;4%
+	db 29, AZUMARILL	;1%
+	
+	map_id STARGLOW_CAVERN_2F
+	db 6 percent ; encounter rate
+	db 29, QUAGSIRE		;30%
+	db 29, POLIWHIRL	;30%
+	db 29, AZUMARILL	;20%
+	db 29, QUAGSIRE		;10%
+	db 29, POLIWHIRL	;5%
+	db 29, AZUMARILL	;4%
+	db 29, AZUMARILL	;1%
+	
+	map_id ROUTE_3_EAST
+	db 6 percent ; encounter rate
+	db 29, POLIWHIRL	;30%
+	db 29, CARVANHA		;30%
+	db 29, FLOATZEL		;20%
+	db 29, POLIWHIRL	;10%
+	db 29, CARVANHA		;5%
+	db 29, FLOATZEL		;4%
+	db 29, FLOATZEL		;1%
+	
+	map_id STARGLOW_VALLEY
+	db 6 percent ; encounter rate
+	db 29, POLIWHIRL	;30%
+	db 29, CARVANHA		;30%
+	db 29, FLOATZEL		;20%
+	db 29, POLIWHIRL	;10%
+	db 29, CARVANHA		;5%
+	db 29, FLOATZEL		;4%
+	db 29, FLOATZEL		;1%
+	
+	map_id ROUTE_4
+	db 6 percent ; encounter rate
+	db 29, POLIWHIRL	;30%
+	db 29, CARVANHA		;30%
+	db 29, FLOATZEL		;20%
+	db 29, POLIWHIRL	;10%
+	db 29, CARVANHA		;5%
+	db 29, FLOATZEL		;4%
+	db 29, FLOATZEL		;1%
+	
+	map_id HUNTERS_THICKET
+	db 8 percent ; encounter rate
+	db 29, POLIWHIRL	;30%
+	db 29, CARVANHA		;30%
+	db 29, FLOATZEL		;20%
+	db 29, POLIWHIRL	;10%
+	db 29, CARVANHA		;5%
+	db 29, FLOATZEL		;4%
+	db 29, FLOATZEL		;1%
+	
+	map_id ROUTE_6
+	db 6 percent ; encounter rate
+	db 29, WINGULL		;30%
+	db 29, CHINCHOU		;30%
+	db 29, HORSEA		;20%
+	db 29, MAREANIE		;10%
+	db 29, MAREANIE		;5%
+	db 29, STARYU		;4%
+	db 29, STARYU		;1%
+	
+	map_id MT_ONWA_B1F
+	db 5 percent ; encounter rate
+	db 16, SLUGMA		;30%
+	db 16, SLUGMA		;30%
+	db 16, SLUGMA		;20%
+	db 16, SLUGMA		;10%
+	db 16, NUMEL		;5%
+	db 16, NUMEL		;4%
+	db 16, NUMEL		;1%
+	
+	map_id MT_ONWA_B2F
+	db 5 percent ; encounter rate
+	db 16, SLUGMA		;30%
+	db 16, SLUGMA		;30%
+	db 16, SLUGMA		;20%
+	db 16, SLUGMA		;10%
+	db 16, NUMEL		;5%
+	db 16, NUMEL		;4%
+	db 16, NUMEL		;1%
+	
+	map_id MT_ONWA_B3F
+	db 5 percent ; encounter rate
+	db 16, SLUGMA		;30%
+	db 16, SLUGMA		;30%
+	db 16, SLUGMA		;20%
+	db 16, SLUGMA		;10%
+	db 16, NUMEL		;5%
+	db 16, NUMEL		;4%
+	db 16, NUMEL		;1%
+	
+	map_id SUNBEAM_ISLAND
+	db 6 percent ; encounter rate
+	db 29, CORSOLA		;30%
+	db 29, HORSEA		;30%
+	db 29, WINGULL		;20%
+	db 29, MAREANIE		;10%
+	db 29, STARYU		;5%
+	db 29, STARYU		;4%
+	db 29, STARYU		;1%
+	
+	map_id SUNBEAM_BEACH
+	db 6 percent ; encounter rate
+	db 29, WINGULL		;30%
+	db 29, CHINCHOU		;30%
+	db 29, HORSEA		;20%
+	db 29, MAREANIE		;10%
+	db 29, MAREANIE		;5%
+	db 29, STARYU		;4%
+	db 29, STARYU		;1%
+	
+	map_id SUNBEAM_RESERVE
+	db 6 percent ; encounter rate
+	db 29, SLOWPOKE		;30%
+	db 29, CORSOLA		;30%
+	db 29, SLOWPOKE		;20%
+	db 29, CORSOLA		;10%
+	db 29, CORSOLA		;5%
+	db 29, CORSOLA		;4%
+	db 29, CORSOLA		;1%
+	
+	map_id SUNBEAM_JUNGLE
+	db 6 percent ; encounter rate
+	db 29, CARVANHA		;30%
+	db 29, AZUMARILL	;30%
+	db 29, CARVANHA		;20%
+	db 29, AZUMARILL	;10%
+	db 29, AZUMARILL	;5%
+	db 29, AZUMARILL	;4%
+	db 29, AZUMARILL	;1%
+	
+	map_id SUNBEAM_JUNGLE_DEEP
+	db 6 percent ; encounter rate
+	db 29, CARVANHA		;30%
+	db 29, AZUMARILL	;30%
+	db 29, CARVANHA		;20%
+	db 29, AZUMARILL	;10%
+	db 29, AZUMARILL	;5%
+	db 29, AZUMARILL	;4%
+	db 29, AZUMARILL	;1%
+
+	map_id ROUTE_8
+	db 6 percent ; encounter rate
+	db 29, POLIWHIRL	;30%
+	db 29, CARVANHA		;30%
+	db 29, FLOATZEL		;20%
+	db 29, POLIWHIRL	;10%
+	db 29, CARVANHA		;5%
+	db 29, FLOATZEL		;4%
+	db 29, FLOATZEL		;1%
+
+	map_id LUSTER_SEWERS_B1F
+	db 6 percent ; encounter rate
+	db 29, GRIMER		;30%
+	db 29, GRIMER		;30%
+	db 29, GRIMER_A		;20%
+	db 29, GRIMER		;10%
+	db 29, GRIMER_A		;5%
+	db 29, GRIMER_A		;4%
+	db 29, GRIMER_A		;1%
+	
+	map_id LUSTER_SEWERS_B2F
+	db 8 percent ; encounter rate
+	db 29, GRIMER		;30%
+	db 29, GRIMER		;30%
+	db 29, GRIMER_A		;20%
+	db 29, GRIMER		;10%
+	db 29, GRIMER_A		;5%
+	db 29, GRIMER_A		;4%
+	db 29, GRIMER_A		;1%
+	
+	map_id LUSTER_SEWERS_MUK_ROOM
+	db 15 percent ; encounter rate
+	db 29, GRIMER		;30%
+	db 29, GRIMER		;30%
+	db 29, GRIMER_A		;20%
+	db 29, GRIMER		;10%
+	db 29, GRIMER_A		;5%
+	db 29, GRIMER_A		;4%
+	db 29, GRIMER_A		;1%
+
 	map_id ROUTE_13
 	db 6 percent ; encounter rate
 	db 29, WINGULL		;30%
@@ -17,6 +277,26 @@
 	db 29, WINGULL		;20%
 	db 29, CORSOLA		;10%
 	db 29, STARYU		;5%
+	db 29, STARYU		;4%
+	db 29, STARYU		;1%
+	
+	map_id SHIMMER_CITY
+	db 6 percent ; encounter rate
+	db 29, WINGULL		;30%
+	db 29, CHINCHOU		;30%
+	db 29, HORSEA		;20%
+	db 29, CORSOLA		;10%
+	db 29, CORSOLA		;5%
+	db 29, STARYU		;4%
+	db 29, STARYU		;1%
+
+	map_id SHIMMER_HARBOR
+	db 6 percent ; encounter rate
+	db 29, WINGULL		;30%
+	db 29, CHINCHOU		;30%
+	db 29, HORSEA		;20%
+	db 29, CORSOLA		;10%
+	db 29, CORSOLA		;5%
 	db 29, STARYU		;4%
 	db 29, STARYU		;1%
 

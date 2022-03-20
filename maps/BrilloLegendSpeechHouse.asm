1d6bc17b335046e4fcd0278446c3bb494136ae94
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2022-03-20 06:36:42 -0400

    Swapped 2 Brillo House Filenames
    Oops

--- maps/BrilloLegendSpeechHouse.asm
+++ maps/BrilloLegendSpeechHouse.asm
@@ -4,31 +4,50 @@ BrilloLegendSpeechHouse_MapScriptHeader:
 	db 0 ; callbacks
 
 	db 2 ; warp events
-	warp_event  2,  7, BRILLO_TOWN, 4
-	warp_event  3,  7, BRILLO_TOWN, 4
+	warp_event  2,  7, BRILLO_TOWN, 3
+	warp_event  3,  7, BRILLO_TOWN, 3
 
 	db 0 ; coord events
 
 	db 0 ; bg events
 
-	db 1 ; object events
-	object_event  2,  4, SPRITE_POKEMANIAC, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_PURPLE, PERSONTYPE_SCRIPT, 0, BrilloLegendSpeechHouseNPC1, -1
+	db 2 ; object events
+	object_event  2,  3, SPRITE_FAT_GUY, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_TEAL, PERSONTYPE_SCRIPT, 0, BrilloLegendSpeechHouseNPC1, -1
+	object_event  5,  4, SPRITE_FISHING_GURU, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, BrilloLegendSpeechHouseNPC2, -1
+	
 	
 BrilloLegendSpeechHouseNPC1:
 	jumptextfaceplayer BrilloLegendSpeechHouseNPC1Text
 	
+BrilloLegendSpeechHouseNPC2:
+	jumptextfaceplayer BrilloLegendSpeechHouseNPC2Text
+	
 BrilloLegendSpeechHouseNPC1Text:
-	text "You like games?"
+	text "The wasteland to"
+	line "the NORTHEAST is"
+	cont "known for intense"
+	cont "sandstorms."
 	
-	para "There's a place in"
-	line "town you might be"
-	cont "interested in…"
+	para "Anyone who gets"
+	line "caught in one is"
+	cont "said to be lost"
+	cont "forever…"
+	done
 	
-	para "All I can say is"
-	line "the password might"
-	cont "start with an “S”."
+BrilloLegendSpeechHouseNPC2Text:
+	text "There is a legend"
+	line "of an explorer who"
+	cont "somehow made it"
+	cont "through the desert"
+	cont "wastes."
 	
-	para "I've already said"
-	line "too much…"
-	done	
+	para "“The steaks in the"
+	line "sand shall guide"
+	cont "you…”"
 	
+	para "“Let the flags be"
+	line "your compass…”"
+	
+	para "…or so the legend"
+	line "goes."
+	done
--- maps/BrilloTradeHouse.asm
+++ maps/BrilloTradeHouse.asm
@@ -4,50 +4,31 @@ BrilloTradeHouse_MapScriptHeader:
 	db 0 ; callbacks
 
 	db 2 ; warp events
-	warp_event  2,  7, BRILLO_TOWN, 3
-	warp_event  3,  7, BRILLO_TOWN, 3
+	warp_event  2,  7, BRILLO_TOWN, 4
+	warp_event  3,  7, BRILLO_TOWN, 4
 
 	db 0 ; coord events
 
 	db 0 ; bg events
 
-	db 2 ; object events
-	object_event  2,  3, SPRITE_FAT_GUY, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_TEAL, PERSONTYPE_SCRIPT, 0, BrilloTradeHouseNPC1, -1
-	object_event  5,  4, SPRITE_FISHING_GURU, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, BrilloTradeHouseNPC2, -1
-	
+	db 1 ; object events
+	object_event  2,  4, SPRITE_POKEMANIAC, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_PURPLE, PERSONTYPE_SCRIPT, 0, BrilloTradeHouseNPC1, -1
 	
 BrilloTradeHouseNPC1:
 	jumptextfaceplayer BrilloTradeHouseNPC1Text
 	
-BrilloTradeHouseNPC2:
-	jumptextfaceplayer BrilloTradeHouseNPC2Text
-	
 BrilloTradeHouseNPC1Text:
-	text "The wasteland to"
-	line "the NORTHEAST is"
-	cont "known for intense"
-	cont "sandstorms."
+	text "You like games?"
 	
-	para "Anyone who gets"
-	line "caught in one is"
-	cont "said to be lost"
-	cont "forever…"
-	done
+	para "There's a place in"
+	line "town you might be"
+	cont "interested in…"
 	
-BrilloTradeHouseNPC2Text:
-	text "There is a legend"
-	line "of an explorer who"
-	cont "somehow made it"
-	cont "through the desert"
-	cont "wastes."
+	para "All I can say is"
+	line "the password might"
+	cont "start with an “S”."
 	
-	para "“The steaks in the"
-	line "sand shall guide"
-	cont "you…”"
+	para "I've already said"
+	line "too much…"
+	done	
 	
-	para "“Let the flags be"
-	line "your compass…”"
-	
-	para "…or so the legend"
-	line "goes."
-	done

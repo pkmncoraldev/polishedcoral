21dd19c85b9930c02387cea7d91e05beeaa57413
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2022-04-17 03:59:21 -0400

    Added Exp Share to Glint Apartments

--- maps/GlintApartmentRight2F.asm
+++ maps/GlintApartmentRight2F.asm
@@ -10,14 +10,35 @@ GlintApartmentRight2F_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 1 ; object events
+	db 2 ; object events
 	person_event SPRITE_BATTLE_GIRL, 5, 2, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, GlintApt22FNpc1, -1
+	person_event SPRITE_ROCKER, 6, 5, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_GREEN, PERSONTYPE_SCRIPT, 0, GlintApt22FNpc2, -1
 
 	const_def 1 ; object constants
 	const GLINT_APT22F_NPC1
 	
 GlintApt22FNpc1:
 	jumptextfaceplayer GlintApt22FNpc1Text
+	
+GlintApt22FNpc2:
+	faceplayer
+	checkevent EVENT_BEAT_STANLEY
+	iffalse .haventbeaten
+	checkevent EVENT_GOT_EXP_SHARE
+	iftrue .got_expshare
+	opentext
+	writetext GlintApt22FNpc2Text2
+	waitbutton
+	verbosegiveitem EXP_SHARE
+	setevent EVENT_GOT_EXP_SHARE
+	closetext
+	end
+	
+.haventbeaten
+	jumptext GlintApt22FNpc2Text1
+	
+.got_expshare
+	jumptext GlintApt22FNpc2Text3
 	
 GlintApt22FNpc1Text:
 	text "GLINT CITY is nice"
@@ -35,4 +56,47 @@ GlintApt22FNpc1Text:
 	cont "with a GYM and a"
 	cont "#MON CENTER in"
 	cont "it!"
+	done
+	
+GlintApt22FNpc2Text1:
+	text "Are you planning"
+	line "on battling the"
+	cont "GYM LEADER?"
+	
+	para "If you win, make"
+	line "sure to come back"
+	cont "and share the"
+	cont "experience with"
+	cont "me!"
+	
+	para "I'll give you some-"
+	line "thing really cool!"
+	done
+	
+GlintApt22FNpc2Text2:
+	text "WOAH!"
+	
+	para "You beat STANLEY?"
+	
+	para "Tell me all about"
+	line "it!"
+	
+	para "…"
+	
+	para "How exciting!"
+	
+	para "Thanks for sharing"
+	line "your experience"
+	cont "with me!"
+	
+	para "This should be"
+	line "useful to a"
+	cont "TRAINER like you!"
+	done
+	
+GlintApt22FNpc2Text3:
+	text "Thanks again for"
+	line "sharing your"
+	cont "experience with"
+	cont "me!"
 	done

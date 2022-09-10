b6d19eed219e47093eb55c9b93f99aca439de531
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2022-09-10 12:40:10 -0400

    Block Route 11 for Demo

--- maps/Route11Gate.asm
+++ maps/Route11Gate.asm
@@ -14,7 +14,13 @@ Route11Gate_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 2 ; object events
+	person_event SPRITE_OFFICER, 7, 3, SPRITEMOVEDATA_STANDING_UP, 1, 1, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, Route11GateEndDemoNPC, -1
+	person_event SPRITE_OFFICER, 7, 4, SPRITEMOVEDATA_STANDING_UP, 1, 1, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, Route11GateEndDemoNPC, -1
+
+	const_def 1 ; object constants
+	const ROUTE_11_GATE_END_DEMO_NPC1
+	const ROUTE_11_GATE_END_DEMO_NPC2
 
 Route11GateCallback:
 	clearevent EVENT_DOUBLE_LANDMARK_SIGN
@@ -22,3 +28,24 @@ Route11GateCallback:
 	clearevent EVENT_IN_SHOPPING_DISTRICT
 	clearevent EVENT_IN_BUSINESS_DISTRICT
 	return
+	
+Route11GateEndDemoNPC:
+	faceplayer
+	opentext
+	writetext Route11GateEndDemoNPCText
+	waitbutton
+	closetext
+	spriteface ROUTE_11_GATE_END_DEMO_NPC1, UP
+	spriteface ROUTE_11_GATE_END_DEMO_NPC2, UP
+	end
+	
+Route11GateEndDemoNPCText:
+	text "Sorry…"
+	
+	para "This area isn't"
+	line "available in the"
+	cont "demo!"
+	
+	para "Come back in the"
+	line "full release!"
+	done

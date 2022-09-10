ba04a117a79d70cb41f2b17e3c8d077295d6961b
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2022-09-10 14:46:33 -0400

    Wild Smeargle Can Hold Paintbrush

--- data/pokemon/base_stats/smeargle.asm
+++ data/pokemon/base_stats/smeargle.asm
@@ -1,16 +1,11 @@
-if DEF(FAITHFUL)
 	db  55,  20,  35,  75,  20,  45
 	;   hp  atk  def  spd  sat  sdf
-else
-	db  55,  20,  35, 100,  20,  45
-	;   hp  atk  def  spd  sat  sdf
-endc
 
 	db NORMAL, NORMAL
 	db 45 ; catch rate
 	db 106 ; base exp
 	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db PAINTBRUSH ; item 2
 	dn FEMALE_50, 3 ; gender, step cycles to hatch
 	dn 7, 7 ; frontpic dimensions
 	db OWN_TEMPO ; ability 1

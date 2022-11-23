3f9ea3646531aaaef556e390661f723cacd4907e
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2022-11-23 05:26:59 -0500

    Fixed Abra's Learnset

--- data/pokemon/evos_attacks.asm
+++ data/pokemon/evos_attacks.asm
@@ -691,8 +691,7 @@ PoliwrathEvosAttacks:
 AbraEvosAttacks:
 	db EVOLVE_LEVEL, 16, KADABRA
 	db 0 ; no more evolutions
-	db 1, SUBSTITUTE
-	db 1, SOFTBOILED_MILK_DRINK_RECOVER
+	db 1, TELEPORT
 	db 0 ; no more level-up moves
 
 KadabraEvosAttacks:

129909a097421ee163dd67f0c477e409824711d0
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2022-12-22 13:48:22 -0500

    Finished Eventide Village NPCs

--- maps/EventideMart.asm
+++ maps/EventideMart.asm
@@ -11,5 +11,41 @@ EventideMart_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 1 ; object events
+	db 3 ; object events
 	mart_clerk_event  1,  3, MARTTYPE_STANDARD, MART_FLICKER
+	person_event SPRITE_COOLTRAINER_F, 2, 8, SPRITEMOVEDATA_STANDING_UP, 1, 1, -1, -1, (1 << 3) | PAL_OW_GREEN, PERSONTYPE_SCRIPT, 0, EventideMartNPC1, -1
+	person_event SPRITE_COOLTRAINER_M, 6, 4, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, (1 << 3) | PAL_OW_TEAL, PERSONTYPE_SCRIPT, 0, EventideMartNPC2, -1
+	
+	const_def 1 ; object constants
+	const EVENTIDE_MART_CLERK
+	const EVENTIDE_MART_NPC1
+	const EVENTIDE_MART_NPC2
+	
+EventideMartNPC1:
+	faceplayer
+	opentext
+	writetext EventideMartNPC1Text
+	waitbutton
+	closetext
+	spriteface EVENTIDE_MART_NPC1, UP
+	end
+	
+EventideMartNPC2:
+	jumptextfaceplayer EventideMartNPC2Text
+	
+EventideMartNPC1Text:
+	text "POTION or"
+	line "SUPER POTION…"
+	
+	para "Which is better"
+	line "bang for your"
+	cont "buck?"
+	done
+	
+EventideMartNPC2Text:
+	text "MOO MOO MILK is"
+	line "much better for"
+	cont "your #MON than"
+	cont "the POTIONs they"
+	cont "sell here."
+	done
--- maps/EventidePokeCenter.asm
+++ maps/EventidePokeCenter.asm
@@ -11,6 +11,66 @@ EventidePokeCenter_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 2 ; object events
+	db 6 ; object events
 	pc_nurse_event  4, 1
 	pc_chansey_event  5, 1
+	person_event SPRITE_GRANNY, 7, 10, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_TEAL, PERSONTYPE_SCRIPT, 0, EventidePokeCenterNPC1, -1
+	person_event SPRITE_SCHOOLGIRL, 5, 9, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, EventidePokeCenterNPC2, -1
+	person_event SPRITE_PICNICKER, 4, 2, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_SCRIPT, 0, EventidePokeCenterNPC3, -1
+	object_event 9, 4, SPRITE_MON_ICON, SPRITEMOVEDATA_POKEMON, 0, LITWICK, -1, -1, PAL_NPC_PURPLE, PERSONTYPE_SCRIPT, 0, EventidePokeCenterLitwick, -1
+
+
+EventidePokeCenterNPC1:
+	jumptextfaceplayer EventidePokeCenterNPC1Text
+	
+EventidePokeCenterNPC2:
+	jumptextfaceplayer EventidePokeCenterNPC2Text
+	
+EventidePokeCenterNPC3:
+	jumptextfaceplayer EventidePokeCenterNPC3Text
+	
+EventidePokeCenterLitwick:
+	opentext
+	writetext EventidePokeCenterLitwickText1
+	cry LITWICK
+	waitsfx
+	buttonsound
+	writetext EventidePokeCenterLitwickText2
+	waitbutton
+	closetext
+	end
+	
+EventidePokeCenterNPC1Text:
+	text "Children often go"
+	line "missing in the"
+	cont "nearby woods."
+	
+	para "How terrible!"
+	done
+	
+EventidePokeCenterNPC2Text:
+	text "I've been so tired"
+	line "lately…"
+	
+	para "I wonder if I'm"
+	line "getting sick…"
+	done
+	
+EventidePokeCenterNPC3Text:
+	text "Phew!"
+	
+	para "The GYM LEADER"
+	line "here is no joke!"
+	
+	para "I might not be"
+	line "ready for her…"
+	done
+	
+EventidePokeCenterLitwickText1:
+	text "LITWICK: Whi! Whi!"
+	done
+	
+EventidePokeCenterLitwickText2:
+	text "It looks fully of"
+	line "energy!"
+	done

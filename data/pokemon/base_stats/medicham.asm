64da1c1b02c2302206ef488d63fe92177b153517
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2022-12-29 22:25:36 -0500

    Fixed Meditite Line Base Stats

--- data/pokemon/base_stats/medicham.asm
+++ data/pokemon/base_stats/medicham.asm
@@ -1,21 +1,21 @@
-	db  85,  73,  70,  67,  73, 115
+	db  60,  60,  75,  80,  60,  75
 	;   hp  atk  def  spd  sat  sdf
 
 	db FIGHTING, PSYCHIC
-	db 75 ; catch rate
+	db 90 ; catch rate
 	db 165 ; base exp
 	db NO_ITEM ; item 1
 	db NO_ITEM ; item 2
 	dn FEMALE_50, 3 ; gender, step cycles to hatch
 	dn 7, 7 ; frontpic dimensions
-	db INSOMNIA ; ability 1
-	db FOREWARN ; ability 2
-	db INNER_FOCUS ; hidden ability
+	db HUGE_POWER ; ability 1
+	db HUGE_POWER ; ability 2
+	db HUGE_POWER ; hidden ability
 	db MEDIUM_FAST ; growth rate
 	dn HUMANSHAPE, HUMANSHAPE ; egg groups
 
 	; ev_yield
-	ev_yield   0,   0,   0,   0,   0,   2
+	ev_yield   0,   0,   0,   2,   0,   0
 	;         hp, atk, def, spd, sat, sdf
 
 	; tmhm
--- data/pokemon/base_stats/meditite.asm
+++ data/pokemon/base_stats/meditite.asm
@@ -1,21 +1,21 @@
-	db  60,  48,  45,  42,  43,  90
+	db  30,  40,  55,  60,  40,  55
 	;   hp  atk  def  spd  sat  sdf
 
 	db FIGHTING, PSYCHIC
-	db 190 ; catch rate
-	db 102 ; base exp
+	db 180 ; catch rate
+	db 91 ; base exp
 	db NO_ITEM ; item 1
 	db NO_ITEM ; item 2
 	dn FEMALE_50, 3 ; gender, step cycles to hatch
 	dn 5, 5 ; frontpic dimensions
-	db INSOMNIA ; ability 1
-	db FOREWARN ; ability 2
-	db INNER_FOCUS ; hidden ability
+	db HUGE_POWER ; ability 1
+	db HUGE_POWER ; ability 2
+	db HUGE_POWER ; hidden ability
 	db MEDIUM_FAST ; growth rate
 	dn HUMANSHAPE, HUMANSHAPE ; egg groups
 
 	; ev_yield
-	ev_yield   0,   0,   0,   0,   0,   1
+	ev_yield   0,   0,   0,   1,   0,   0
 	;         hp, atk, def, spd, sat, sdf
 
 	; tmhm

d2dbf60510f4538a47da83ea9fe3ab2e8fc6f31a
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2023-02-08 07:41:56 -0500

    More Placeholder Mon Sprites

--- data/pokemon/base_stats/mareanie.asm
+++ data/pokemon/base_stats/mareanie.asm
@@ -7,7 +7,7 @@
 	db POISON_BARB ; item 1
 	db POISON_BARB ; item 2
 	dn FEMALE_50, 3 ; gender, step cycles to hatch
-	dn 5, 5 ; frontpic dimensions
+	dn 6, 6 ; frontpic dimensions
 	db MERCILESS ; ability 1
 	db LIMBER ; ability 2
 	db REGENERATOR ; hidden ability
--- data/pokemon/base_stats/scrafty.asm
+++ data/pokemon/base_stats/scrafty.asm
@@ -7,7 +7,7 @@
 	db NO_ITEM ; item 1
 	db NO_ITEM ; item 2
 	dn FEMALE_50, 3 ; gender, step cycles to hatch
-	dn 5, 5 ; frontpic dimensions
+	dn 7, 7 ; frontpic dimensions
 	db SHED_SKIN ; ability 1
 	db MOXIE ; ability 2
 	db INTIMIDATE ; hidden ability

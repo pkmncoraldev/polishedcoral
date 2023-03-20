34f2753e3f5e755bf7ccb20c0c31ee5b1c5e65a9
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2023-03-20 01:03:40 -0400

    Unblock Route 11 From Demo

--- maps/Route11Gate.asm
+++ maps/Route11Gate.asm
@@ -14,13 +14,12 @@ Route11Gate_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 2 ; object events
-	person_event SPRITE_OFFICER, 7, 3, SPRITEMOVEDATA_STANDING_UP, 1, 1, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, Route11GateEndDemoNPC, -1
-	person_event SPRITE_OFFICER, 7, 4, SPRITEMOVEDATA_STANDING_UP, 1, 1, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, Route11GateEndDemoNPC, -1
+	db 1 ; object events
+	person_event SPRITE_OFFICER, 4, 0, SPRITEMOVEDATA_STANDING_RIGHT, 1, 1, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, Route11GateOfficer, -1
 
 	const_def 1 ; object constants
-	const ROUTE_11_GATE_END_DEMO_NPC1
-	const ROUTE_11_GATE_END_DEMO_NPC2
+	const ROUTE_11_GATE_OFFICER
+
 
 Route11GateCallback:
 	clearevent EVENT_DOUBLE_LANDMARK_SIGN
@@ -29,75 +28,19 @@ Route11GateCallback:
 	clearevent EVENT_IN_BUSINESS_DISTRICT
 	return
 	
-Route11GateEndDemoNPC:
-	faceplayer
-	opentext
-	writetext Route11GateEndDemoNPCText1
-	buttonsound
-	farwritetext StdBlankText
-	pause 6
-	checkitem SKATEBOARD
-	iftrue .ask_skatepark
-.return
-	farwritetext StdBlankText
-	pause 6
-	checkevent EVENT_GOT_HM03_SURF
-	iftrue .ask_end_demo
-	writetext Route11GateEndDemoNPCText2
-	waitbutton
-	jump .endtext
-.ask_skatepark
-	writetext Route11GateEndDemoNPCText3
-	yesorno
-	iffalse .return
-	closetext
-	warpfacing LEFT, SKATEPARK, 17, 26
-	end
-.ask_end_demo
-	writetext Route11GateEndDemoNPCText4
-	yesorno
-	iffalse .demo_end_no
-	credits
-.demo_end_no
-	writetext Route11GateEndDemoNPCText5
-	waitbutton
-.endtext
-	closetext
-	spriteface ROUTE_11_GATE_END_DEMO_NPC1, UP
-	spriteface ROUTE_11_GATE_END_DEMO_NPC2, UP
-	end
+Route11GateOfficer:
+	jumptextfaceplayer Route11GateOfficerText
+
+Route11GateOfficerText:
+	text "The ROUTE 11 PARK"
+	line "is through those"
+	cont "doors."
 	
-Route11GateEndDemoNPCText1:
-	text "Sorry…"
+	para "It may seem like"
+	line "like you're still"
+	cont "in the city,"
+	cont "but watch out!"
 	
-	para "This area isn't"
-	line "available in the"
-	cont "demo!"
-	done
-	
-Route11GateEndDemoNPCText2:
-	text "There's still"
-	line "more to do in"
-	cont "LUSTER CITY."
-	
-	para "Come talk to me"
-	line "when you finish"
-	cont "up!"
-	done
-	
-Route11GateEndDemoNPCText3:
-	text "Would you like"
-	line "to warp to the"
-	cont "SKATEPARK?"
-	done
-	
-Route11GateEndDemoNPCText4:
-	text "Would you like"
-	line "to end the demo?"
-	done
-	
-Route11GateEndDemoNPCText5:
-	text "Come talk to me"
-	line "when you want to"
-	cont "end the demo."
+	para "Plenty of TRAINERS"
+	line "will want a fight!"
 	done

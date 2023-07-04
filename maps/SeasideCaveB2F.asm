acc591c5fede13498dec1fbcbabc307df4733bc5
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2023-07-04 00:49:00 -0400

    Added Drakloak Encounter

--- maps/SeasideCaveB2F.asm
+++ maps/SeasideCaveB2F.asm
@@ -10,4 +10,40 @@ SeasideCaveB2F_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 1 ; object events
+	object_event  8,  8, SPRITE_MON_ICON, SPRITEMOVEDATA_POKEMON, 0, DRAKLOAK, -1, -1, (1 << 3) | PAL_NPC_GREEN, PERSONTYPE_SCRIPT, 0, SeasideCaveB2FDrakloak, EVENT_SEASIDE_CAVE_B2F_DRAKLOAK_GONE
+
+	const_def 1 ; object constants
+	const SEASIDE_CAVE_B2F_DRAKLOAK
+
+SeasideCaveB2FDrakloak:
+	opentext
+	writetext SeasideCaveB2FDrakloakText
+	cry DRAKLOAK
+	waitbutton
+	closetext
+	waitsfx
+	loadwildmon DRAKLOAK, 46
+	writecode VAR_BATTLETYPE, BATTLETYPE_LEGENDARY
+	startbattle
+	disappear SEASIDE_CAVE_B2F_DRAKLOAK
+	reloadmapafterbattle
+	setevent EVENT_SEASIDE_CAVE_B2F_DRAKLOAK_GONE
+	checkcode VAR_MONJUSTCAUGHT
+	if_equal LEDIAN, .CaughtDrakloak
+	opentext
+	writetext SeasideCaveB2FDrakloakTextGone
+	waitbutton
+	closetext
+.CaughtDrakloak
+	writecode VAR_BATTLETYPE, BATTLETYPE_NORMAL
+	end
+	
+SeasideCaveB2FDrakloakText:
+	text "DRA-DRA!"
+	done
+	
+SeasideCaveB2FDrakloakTextGone:
+	text "DRAKLOAK fled"
+	line "into the water…"
+	done

7e24e04e379fb16aee1e8f1f5bd0ff740b6444f0
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2023-08-30 22:58:00 -0400

    Edited Cue Ball OW Sprite

--- data/sprites/sprites.asm
+++ data/sprites/sprites.asm
@@ -131,7 +131,7 @@ SpriteHeaders:
 	overworld_sprite KageSpriteGFX, WALKING_SPRITE, PAL_OW_GREEN
 	overworld_sprite KageWaterSpriteGFX, STANDING_SPRITE, PAL_OW_GREEN
 	overworld_sprite LockeSpriteGFX, WALKING_SPRITE, PAL_OW_GREEN
-	overworld_sprite RoughneckSpriteGFX, WALKING_SPRITE, PAL_OW_BLUE
+	overworld_sprite CueballSpriteGFX, WALKING_SPRITE, PAL_OW_BLUE
 	overworld_sprite SageSpriteGFX, WALKING_SPRITE, PAL_OW_BLUE
 	overworld_sprite SailorSpriteGFX, WALKING_SPRITE, PAL_OW_BLUE
 	overworld_sprite SchoolboySpriteGFX, WALKING_SPRITE, PAL_OW_BLUE

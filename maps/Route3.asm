843e9d87fa01e9e02d66948acdc5110fd753c3d5
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2023-11-08 21:48:09 -0500

    Added Patches to Top of Cliff on Route 3

--- maps/Route3.asm
+++ maps/Route3.asm
@@ -1,7 +1,8 @@
 Route3_MapScriptHeader:
 	db 0 ; scene scripts
 
-	db 0 ; callbacks
+	db 1 ; callbacks
+	callback MAPCALLBACK_TILES, Route3Callback
 
 	db 3 ; warp events
 	warp_def  7, 20, 1, STARGLOW_CAVERN_1F
@@ -23,10 +24,11 @@ Route3_MapScriptHeader:
 	signpost 9, 0, SIGNPOST_READ, Route3_sign2
 	signpost 18, 34, SIGNPOST_READ, Route3_sign3
 
-	db 14 ; object events
+	db 15 ; object events
 	person_event SPRITE_YOUNGSTER, 8, 10, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_GENERICTRAINER, 2, TrainerRoute3West_1, -1
 	person_event SPRITE_COOLTRAINER_M, 16, 30, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_GENERICTRAINER, 2, TrainerRoute3West_2, -1
-	person_event SPRITE_BATTLE_GIRL, 13, 36, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_SCRIPT, 0, TrainerRoute3WestNpc, -1
+	person_event SPRITE_BATTLE_GIRL, 13, 36, SPRITEMOVEDATA_WANDER, 0, 1, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_SCRIPT, 0, TrainerRoute3WestNpc, -1
+	person_event SPRITE_PATCHES,  8, 38, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, (1 << 3) | PAL_OW_PURPLE, PERSONTYPE_SCRIPT, 0, TrainerRoute3WestPatches, -1
 	itemball_event 46, 30, RARE_CANDY, 1, EVENT_ROUTE_3_RARE_CANDY
 	cuttree_event 24, 11, EVENT_ROUTE_3_CUT_TREE_1
 	cuttree_event 23, 26, EVENT_ROUTE_3_CUT_TREE_2
@@ -43,6 +45,7 @@ Route3_MapScriptHeader:
 	const ROUTE3WEST_TRAINER1
 	const ROUTE3WEST_TRAINER2
 	const ROUTE3WEST_NPC
+	const ROUTE3WEST_PATCHES
 	const ROUTEWEST_POKEBALL
 	const ROUTEWEST_CUT_TREE1
 	const ROUTEWEST_CUT_TREE2
@@ -54,6 +57,15 @@ Route3_MapScriptHeader:
 	const ROUTEWEST_ROCK_SMASH5
 	const ROUTEWEST_ROCK_SMASH6
 	
+	
+Route3Callback:
+	checkevent EVENT_BEAT_CHARLIE
+	iffalse .skip
+	checkflag ENGINE_GOT_ROCK_CLIMB
+	iffalse .skip
+	moveperson ROUTE3WEST_PATCHES, -5, -5
+.skip
+	return
 	
 Route3MakeSilverBrown:
 	setevent EVENT_ROUTE_3_ROCKS_BROWN
@@ -121,6 +133,24 @@ TrainerRoute3WestNpcText:
 	cont "up to, but my"
 	cont "#MON can't"
 	cont "climb rocks…"
+	done
+	
+TrainerRoute3WestPatches:
+	jumptextfaceplayer TrainerRoute3WestPatchesText
+
+TrainerRoute3WestPatchesText:
+	text "You cheated not"
+	line "only the game,"
+	cont "but yourself."
+	
+	para "You didn't grow."
+	
+	para "You didn't"
+	line "improve."
+	
+	para "You took a short-"
+	line "cut and gained"
+	cont "nothing."
 	done
 
 Route3_sign:

7603b00c924a1470efd1ca79936dae3f0ee9c5db
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2024-01-10 00:38:34 -0500

    Replaced Drowzee Sprite with SourCrystal Sprite

--- gfx/pokemon/drowzee/frames.asm
+++ gfx/pokemon/drowzee/frames.asm
@@ -3,19 +3,34 @@
 	dw .frame3
 	dw .frame4
 	dw .frame5
+	dw .frame6
+	dw .frame7
+	dw .frame8
+	dw .frame9
 .frame1
 	db $00 ; bitmask
-	db $24, $25, $26, $27, $28
+	db $24
 .frame2
 	db $01 ; bitmask
-	db $29, $2a, $2b, $2c, $2d, $24, $25, $26, $27, $28
+	db $25, $26, $27, $28, $29, $2a
 .frame3
 	db $02 ; bitmask
-	db $29, $2a, $2b, $2c, $2d, $2e, $2f, $30, $27, $31, $32
+	db $2b, $2c, $2d, $2e, $2f, $30, $25, $26, $27, $28, $29, $2a
 .frame4
 	db $03 ; bitmask
-	db $2e, $2f, $30, $27, $31, $32
+	db $2b, $2c, $2d, $2e, $2f, $30, $24, $31, $32, $33, $28, $34, $35
 .frame5
+	db $03 ; bitmask
+	db $36, $37, $38, $39, $3a, $3b, $24, $3c, $3d, $3e, $3f, $40, $41
+.frame6
+	db $02 ; bitmask
+	db $36, $37, $38, $39, $3a, $3b, $24, $42, $43, $44, $45, $46
+.frame7
+	db $02 ; bitmask
+	db $00, $47, $48, $39, $49, $4a, $24, $42, $43, $44, $45, $46
+.frame8
 	db $04 ; bitmask
-	db $33, $34, $35, $36
-; 0xd561c
+	db $00, $47, $48, $39, $49, $4a, $24
+.frame9
+	db $05 ; bitmask
+	db $4b, $4c

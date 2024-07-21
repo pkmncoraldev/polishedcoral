ff3b58987309c6683fce755382474e017fc08f40
pkmncoraldev <37190107+pkmncoraldev@users.noreply.github.com>
2024-07-20 22:56:01 -0400

    Slight Edits to Teachers

--- maps/DaybreakPokemonTrainerSchool.asm
+++ maps/DaybreakPokemonTrainerSchool.asm
@@ -17,7 +17,7 @@ DaybreakPokemonTrainerSchool_MapScriptHeader:
 	bg_event 0, 1, SIGNPOST_ITEM + TAPE_PLAYER, EVENT_MUSIC_DAYBREAK_VILLAGE
 
 	db 6 ; object events
-	person_event SPRITE_TEACHER, 2, 4, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, (1 << 3) | PAL_OW_PURPLE, PERSONTYPE_SCRIPT, 0, DaybreakPokemonTrainerSchoolTeacher, -1
+	person_event SPRITE_TEACHER, 2, 4, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, (1 << 3) | PAL_OW_GREEN, PERSONTYPE_SCRIPT, 0, DaybreakPokemonTrainerSchoolTeacher, -1
 	person_event SPRITE_YOUNGSTER, 5, 4, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, DaybreakPokemonTrainerSchoolNPC1, -1
 	person_event SPRITE_GAMEBOY_KID, 11, 5, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_SCRIPT, 0, DaybreakPokemonTrainerSchoolNPC2, -1
 	person_event SPRITE_CUTE_GIRL, 7, 2, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, (1 << 3) | PAL_OW_TEAL, PERSONTYPE_SCRIPT, 0, DaybreakPokemonTrainerSchoolNPC3, -1

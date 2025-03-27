9f3867827ed559cec2e8f6f5f4fd365a5d607255
pkmncoraldev <pkmncoraldev@gmail.com>
2025-03-27 19:50:23 -0400

    Otto's Dialog

--- maps/Route28OttosTent.asm
+++ maps/Route28OttosTent.asm
@@ -16,6 +16,7 @@ Route28OttosTent_MapScriptHeader:
 	object_event  1,  3, SPRITE_MON_ICON, SPRITEMOVEDATA_POKEMON, 0, TOGEPI, -1, -1, PAL_NPC_RED, PERSONTYPE_SCRIPT, 0, Route28OttosTentTogepi, -1
 	
 Route28OttosTentOtto:
+	faceplayer
 	opentext
 	checkevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_8
 	iftrue .just_shopped
@@ -53,6 +54,7 @@ Route28OttosTentOtto:
 	verticalmenu
 	if_equal $1, .rest
 	if_equal $2, .shop
+	if_equal $3, .advice
 	closewindow
 	writetext Route28OttosTentOttoText7
 	waitbutton
@@ -60,8 +62,6 @@ Route28OttosTentOtto:
 	jump .end
 .rest
 	closewindow
-	farwritetext StdBlankText
-	pause 6
 	writetext Route28OttosTentOttoText8
 	waitbutton
 	closetext
@@ -96,6 +96,13 @@ Route28OttosTentOtto:
 	waitbutton
 	closetext
 	setevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_8
+	jump .end
+.advice
+	closewindow
+	writetext Route28OttosTentOttoText11
+	waitbutton
+	closetext
+	setevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_6
 .end
 	setevent EVENT_TALKED_TO_OTTO
 	clearevent EVENT_OTTO_TALKS_ABOUT_RIVAL
@@ -104,60 +111,160 @@ Route28OttosTentOtto:
 .Route28OttosTentOttoMenuData
 	db $40 ; flags
 	db 00, 00 ; start coords
-	db 07, 19 ; end coords
+	db 09, 12 ; end coords
 	dw .MenuData2Route28OttosTentOtto
 	db 1 ; default option
 ; 0x48e04
 
 .MenuData2Route28OttosTentOtto:
 	db $80 ; flags
-	db 3 ; items
+	db 4 ; items
 	db "REST@"
-	db "SHOP@"
-	db "NO THANKS@"
+	db "HOLISTICS@"
+	db "ADVICE@"
+	db "BYE@"
 
 Route28OttosTentOttoText1:
-	text "NORMAL"
+	text "Hey, hey!"
+	
+	para "Good to see ya,"
+	line "again!"
 	done
 
 Route28OttosTentOttoText2:
-	text "DONE SHOP"
+	text "You don't have"
+	line "any allergies,"
+	cont "right?"
 	done
 	
 Route28OttosTentOttoText3:
-	text "REG. INTRO"
+	text "Hey, maaan!"
+	line "The name's OTTO!"
+	
+	para "Come on in!"
+	
+	para "My travels bring"
+	line "me all over the"
+	cont "place, man."
+	
+	para "No matter where I"
+	line "go, Mother Nature"
+	cont "takes good care."
+	
+	para "You need a spot"
+	line "to crash, or some"
+	cont "holistics, just"
+	cont "say so, my friend."
+	
+	para "I'm always open"
+	line "to share the love"
+	cont "with a fellow"
+	cont "traveler."
 	done
 
 Route28OttosTentOttoText4:
-	text "WHAT DO YOU WANT?"
+	text "So, whatcha need,"
+	line "man?"
 	done
 	
 Route28OttosTentOttoText5:
-	text "RIVAL INTRO"
+	text "Woah, hey!"
+	
+	para "I don't want no"
+	line "trouble, man!"
+	
+	para "…"
+	
+	para "You aren't with"
+	line "those other dudes?"
+	
+	para "Oh, groovy, man."
+	
+	para "I offered to let"
+	line "them crash for a"
+	cont "while, but they"
+	cont "were in a rush."
+	
+	para "Before they left,"
+	line "they stole some"
+	cont "of my herbs, too…"
+	
+	para "Not cool!"
+	
+	para "Anyway, the name's"
+	line "OTTO."
+	
+	para "My travels bring"
+	line "me all over the"
+	cont "place, man."
+	
+	para "No matter where I"
+	line "go, Mother Nature"
+	cont "takes good care."
+	
+	para "You need a spot"
+	line "to crash, or some"
+	cont "holistics, just"
+	cont "say so, my friend."
+	
+	para "I'm always open"
+	line "to share the love"
+	cont "with a fellow"
+	cont "traveler."
 	done
 	
 Route28OttosTentOttoText6:
-	text "SHOP"
+	text "Lookin' for some"
+	line "holistics, man?"
 	done
 	
 Route28OttosTentOttoText7:
-	text "NO THANKS"
+	text "Don't be a"
+	line "stranger, man."
 	done
 	
 Route28OttosTentOttoText8:
-	text "REST"
+	text "No worries."
+	
+	para "Soy sleeping bag,"
+	line "yo sleeping bag!"
 	done
 	
 Route28OttosTentOttoText9:
-	text "JUST SLEPT"
+	text "Man, you were out"
+	line "like light!"
 	done
 	
 Route28OttosTentOttoText10:
-	text "JUST SHOPPED"
+	text "Need anything"
+	line "else?"
+	
+	para "Can never have"
+	line "too many herbs,"
+	cont "you know?"
 	done
 	
 Route28OttosTentOttoText11:
-	text "SKIP INTRO"
+	text "You want my"
+	line "advice, dude?"
+	
+	para "Stay away from"
+	line "that crazy town"
+	cont "up NORTH."
+	
+	para "BRIGHTBURG."
+	
+	para "Those people may"
+	line "just seem rude,"
+	cont "but it's more than"
+	cont "that…"
+	
+	para "I'm telling you,"
+	line "all isn't what it"
+	cont "seems…"
+	
+	para "That place freaks"
+	line "me out, man!"
 	done
 
 OttoAsm:

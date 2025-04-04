9597208a17ad66c8f146f81c326519389cd73b88
pkmncoraldev <pkmncoraldev@gmail.com>
2025-04-04 00:14:45 -0400

    Assigned Missing FishGroups

--- data/wild/fish.asm
+++ data/wild/fish.asm
@@ -1,5 +1,5 @@
 FishGroups: ; 92488
-	dbwww 75 percent + 1, .Glint_Old,     		 .Route_3_Pond_Good,     .Route_3_Pond_Super
+	dbwww 75 percent + 1, .Route_3_Pond_Old,     .Route_3_Pond_Good,     .Route_3_Pond_Super
 	dbwww 75 percent + 1, .Glint_Old,        	 .Glint_Good,        	 .Glint_Super
 	dbwww 75 percent + 1, .Starglow_Old,         .Starglow_Good,         .Starglow_Super
 	dbwww 75 percent + 1, .Lake_Old,             .Lake_Good,             .Lake_Super
@@ -9,93 +9,153 @@ FishGroups: ; 92488
 	dbwww 75 percent + 1, .Grove_Old,        	 .Grove_Good,        	 .Grove_Super
 	dbwww 75 percent + 1, .Route_3_Pond_Old,     .Route_3_Pond_Good,     .Route_3_Pond_Super
 	dbwww 75 percent + 1, .Sunbeam_Reserve_Old,  .Sunbeam_Reserve_Good,  .Sunbeam_Reserve_Super
+	dbwww 75 percent + 1, .Sewer_Old,  			 .Sewer_Good, 			 .Sewer_Super
+	dbwww 75 percent + 1, .Temple_Old,  		 .Temple_Good, 			 .Temple_Super
+	dbwww 75 percent + 1, .Autumn_Old,  		 .Autumn_Good, 			 .Autumn_Super
 	
 .Glint_Old:
 	db  70 percent, MAGIKARP,   10	;70%
 	db  85 percent, MAGIKARP,   10	;15%
 	db 100 percent, MAGIKARP,   10	;15%
-.Glint_Super:
 .Glint_Good:
 	db  35 percent, POLIWAG,  	11	;40%
 	db  70 percent, BUIZEL,	    11	;30%
 	db  90 percent, BUIZEL,     11	;20%
 	db 100 percent, POLIWAG, 	11	;10%
+.Glint_Super:
+	db  35 percent, POLIWHIRL, 	34	;40%
+	db  70 percent, FLOATZEL,   34	;30%
+	db  90 percent, FLOATZEL,   34	;20%
+	db 100 percent, POLIWHIRL, 	34	;10%
 	
 .Starglow_Old:
 	db  70 percent, MAGIKARP,   10	;70%
 	db  85 percent, MAGIKARP,   10	;15%
 	db 100 percent, MAGIKARP,   10	;15%
-.Starglow_Super:
 .Starglow_Good:
 	db  35 percent, POLIWAG,  	11	;40%
 	db  70 percent, CARVANHA,   11	;30%
 	db  90 percent, MAGIKARP,   11	;20%
 	db 100 percent, POLIWAG, 	11	;10%
+.Starglow_Super:
+	db  35 percent, POLIWHIRL,  34	;40%
+	db  70 percent, SHARPEDO,   34	;30%
+	db  90 percent, GYARADOS,   34	;20%
+	db 100 percent, POLIWHIRL, 	34	;10%
 
 .Lake_Old:
 	db  70 percent, MAGIKARP,   10	;70%
 	db  85 percent, MAGIKARP,   10	;15%
 	db 100 percent, MAGIKARP,   10	;15%
-.Lake_Super:
 .Lake_Good:
 	db  40 percent, MARILL,     15	;40%
 	db  70 percent, CARVANHA,  	15	;30%
 	db  90 percent, WOOPER,     15	;20%
 	db 100 percent, CARVANHA,   15	;10%
+.Lake_Super:
+	db  40 percent, AZUMARILL,  34	;40%
+	db  70 percent, SHARPEDO,  	34	;30%
+	db  90 percent, QUAGSIRE,   34	;20%
+	db 100 percent, SHARPEDO,   34	;10%
 	
 .Sunbeam_Old:
 	db  70 percent, MAGIKARP,   10	;70%
 	db  85 percent, MAGIKARP,   10	;15%
 	db 100 percent, MAGIKARP,   10	;15%
-.Sunbeam_Super:
 .Sunbeam_Good:
-	db  40 percent, STARYU,   16	;40%
-	db  70 percent, CARVANHA,  	16	;30%
-	db  90 percent, CARVANHA,   16	;20%
+	db  40 percent, STARYU,     16	;40%
+	db  70 percent, HORSEA,  	16	;30%
+	db  90 percent, HORSEA,   	16	;20%
 	db 100 percent, CORSOLA, 	16	;10%
+.Sunbeam_Super:
+	db  40 percent, STARYU,     34	;40%
+	db  70 percent, MAREANIE,  	34	;30%
+	db  90 percent, MAREANIE,   34	;20%
+	db 100 percent, CORSOLA, 	34	;10%
 	
 .Cave_Old:
 	db  70 percent, MAGIKARP,   10	;70%
 	db  85 percent, MAGIKARP,   10	;15%
 	db 100 percent, MAGIKARP,   10	;15%
-.Cave_Super:
 .Cave_Good:
 	db  40 percent, WOOPER,   	11	;40%
 	db  70 percent, POLIWAG,  	11	;30%
 	db  90 percent, POLIWAG,   	11	;20%
 	db 100 percent, STARYU, 	11	;10%
+.Cave_Super:
+	db  40 percent, QUAGSIRE,   34	;40%
+	db  70 percent, POLIWHIRL,  34	;30%
+	db  90 percent, POLIWHIRL,  34	;20%
+	db 100 percent, STARYU, 	34	;10%
 	
 .Jungle_Old:
 	db  70 percent, MAGIKARP,   18	;70%
 	db  85 percent, MAGIKARP,   18	;15%
 	db 100 percent, MAGIKARP,   18	;15%
-.Jungle_Super:
 .Jungle_Good:
 	db  40 percent, CARVANHA,   18	;40%
 	db  70 percent, MARILL,  	18	;30%
 	db  90 percent, STARYU,   	18	;20%
 	db 100 percent, GYARADOS, 	20	;10%
+.Jungle_Super:
+	db  40 percent, SHARPEDO,   34	;40%
+	db  70 percent, AZUMARILL,  34	;30%
+	db  90 percent, STARYU,   	34	;20%
+	db 100 percent, GYARADOS, 	34	;10%
 
 .Grove_Old:
 	db  70 percent, MAGIKARP,   18	;70%
 	db  85 percent, MAGIKARP,   18	;15%
 	db 100 percent, MAGIKARP,   18	;15%
-.Grove_Super:
 .Grove_Good:
 	db  40 percent, MARILL,   	20	;40%
 	db  70 percent, POLIWAG,  	20	;30%
 	db  90 percent, MARILL,   	20	;20%
 	db 100 percent, POLIWAG, 	20	;10%
+.Grove_Super:
+	db  40 percent, AZUMARILL,  34	;40%
+	db  70 percent, POLIWHIRL,  34	;30%
+	db  90 percent, AZUMARILL, 	34	;20%
+	db 100 percent, POLIWHIRL, 	34	;10%
 	
 .Route_3_Pond_Old:
 .Route_3_Pond_Good:
-.Route_3_Pond_Super:
 	db 100 percent, DRATINI,   	20	;100%
+.Route_3_Pond_Super:
+	db 100 percent, DRATINI,   	40	;100%
 	
 .Sunbeam_Reserve_Old:
 .Sunbeam_Reserve_Good:
-.Sunbeam_Reserve_Super:
 	db 100 percent, SLOWPOKE,   15	;100%
+.Sunbeam_Reserve_Super:
+	db 100 percent, SLOWPOKE,   34	;100%
+
+.Sewer_Old:
+.Sewer_Good:
+	db 100 percent, GRIMER,		25	;100%
+.Sewer_Super:
+	db 100 percent, GRIMER,		34	;100%
+	
+.Temple_Old:
+.Temple_Good:
+	db 100 percent, CORSOLA,  	25	;100%
+.Temple_Super:
+	db 100 percent, CORSOLA,  	34	;100%
+
+.Autumn_Old:
+	db  70 percent, MAGIKARP,   25	;70%
+	db  85 percent, MAGIKARP,   25	;15%
+	db 100 percent, MAGIKARP,   25	;15%
+.Autumn_Good:
+	db  40 percent, AZUMARILL,  34	;40%
+	db  70 percent, POLIWHIRL,  34	;30%
+	db  90 percent, POLIWHIRL, 	34	;20%
+	db 100 percent, LAPRAS, 	34	;10%
+.Autumn_Super:
+	db  40 percent, AZUMARILL,  34	;40%
+	db  70 percent, POLIWHIRL,  34	;30%
+	db  90 percent, POLIWHIRL, 	34	;20%
+	db 100 percent, LAPRAS, 	34	;10%
 
 FishItems:
 	db PEARL

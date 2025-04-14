4d25fdd9ba261ea1433968b4a4c20fc9b67b2792
SoupPotato <toomanyluigis11@gmail.com>
2025-04-14 06:19:04 +0100

    Added support for dbsprite

--- macros/data.asm
+++ macros/data.asm
@@ -109,6 +109,10 @@ dsprite: MACRO
 	db (\1 * 8) % $100 + \2, (\3 * 8) % $100 + \4, \5, \6
 endm
 
+dbsprite: MACRO 
+; x tile, y tile, x pixel, y pixel, vtile offset, attributes
+	db (\2 * TILE_WIDTH) % $100 + \4, (\1 * TILE_WIDTH) % $100 + \3, \5, \6
+endm
 
 bcd: MACRO
 	rept _NARG

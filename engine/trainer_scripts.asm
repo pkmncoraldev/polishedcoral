e44ada0c3e5e0b0c18e09109197d0837426c01ec
pkmncoraldev <pkmncoraldev@gmail.com>
2025-05-11 14:06:50 -0400

    Fixed Airport Guards Crashing When Talked To

--- engine/trainer_scripts.asm
+++ engine/trainer_scripts.asm
@@ -3,15 +3,32 @@ TalkToTrainerScript:: ; 0xbe66a
 	trainerflagaction CHECK_FLAG
 	iftrue AlreadyBeatenTrainerScript
 	loadmemtrainer
+	callasm CheckTrainerClass
+	if_equal PLAYER_CORY, TalkToAirportGuardScript
 	encountermusic
 	jump StartBattleWithMapTrainerScript
+	
+TalkToAirportGuardScript:
+	faceobject PLAYER, LAST_TALKED
+	opentext
+	trainertext $0
+	waitbutton
+	waitsfx
+	closetext
+	special FadeOutPalettes
+	playsound SFX_EXIT_BUILDING
+	waitsfx
+	loadvar wOtherTrainerClass, 0
+	domaptrigger ONWA_INTL_AIRPORT, $1
+	warpfacing DOWN, ONWA_INTL_AIRPORT, $0d, $00
+	end
 
 SeenByTrainerScript:: ; 0xbe675
 	special Special_StopRunning
 	callasm SeenByTrainerLandmarkTimer
 	loadmemtrainer
 	callasm CheckTrainerClass
-	if_equal PLAYER_CORY, AirportGuardScript
+	if_equal PLAYER_CORY, SeenByAirportGuardScript
 	encountermusic
 	showemote EMOTE_SHOCK, LAST_TALKED, 30
 	callasm TrainerWalkToPlayer
@@ -21,7 +38,7 @@ SeenByTrainerScript:: ; 0xbe675
 	faceobject PLAYER, LAST_TALKED
 	jump StartBattleWithMapTrainerScript
 
-AirportGuardScript:
+SeenByAirportGuardScript:
 	clearevent EVENT_AIRPORT_FENCE
 	playsound SFX_PAY_DAY
 	showemote EMOTE_SHOCK, LAST_TALKED, 30

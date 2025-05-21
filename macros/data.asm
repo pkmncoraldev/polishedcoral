55fef0fb03b9a76034b76c5ea0ecaabb53a918f9
pkmncoraldev <pkmncoraldev@gmail.com>
2025-05-21 04:20:49 -0400

    Fixed Build Errors

--- macros/data.asm
+++ macros/data.asm
@@ -109,6 +109,10 @@ MACRO dsprite
 	db (\1 * 8) % $100 + \2, (\3 * 8) % $100 + \4, \5, \6
 endm
 
+MACRO dbsprite
+; x tile, y tile, x pixel, y pixel, vtile offset, attributes
+	db (\2 * TILE_WIDTH) % $100 + \4, (\1 * TILE_WIDTH) % $100 + \3, \5, \6
+endm
 
 MACRO bcd
 	rept _NARG

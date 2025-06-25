4081010e398ca7633ddf699b054cd4954738ed40
pkmncoraldev <pkmncoraldev@gmail.com>
2025-06-25 06:12:36 -0400

    Route 10 East NeverMeltIce

--- maps/Route10East.asm
+++ maps/Route10East.asm
@@ -36,10 +36,11 @@ Route10East_MapScriptHeader:
 	signpost 20,  9, SIGNPOST_READ, Route10EastWood
 	signpost 21,  9, SIGNPOST_READ, Route10EastWood
 
-	db 3 ; object events
+	db 4 ; object events
 	object_event 11, 24, SPRITE_CAMPFIRE, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, (1 << 3) | PAL_OW_BROWN, PERSONTYPE_SCRIPT, 0, Route10EastCampfire, EVENT_HIDE_OW_OBJECTS_BROWN
 	object_event 11, 24, SPRITE_CAMPFIRE, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, (1 << 3) | PAL_OW_TEAL, PERSONTYPE_SCRIPT, 0, Route10EastCampfire, EVENT_HIDE_OW_OBJECTS_TEAL
 	tmhmball_event 11, 21, TM_BLIZZARD, EVENT_ROUTE_10_EAST_POKE_BALL_BLIZZARD
+	itemball_event 19, 21, REVIVE, 1, EVENT_ROUTE_10_EAST_POKE_BALL_NEVERMELTICE
 
 	const_def 1 ; object constants
 	const ROUTE_10_EAST_FIRE_1

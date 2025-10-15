9a1099250c2300bda09894cada0f86a3b2d1822b
pkmncoraldev <pkmncoraldev@gmail.com>
2025-10-15 04:23:32 -0400

    Town Map Updates

--- data/maps/landmarks.asm
+++ data/maps/landmarks.asm
@@ -5,79 +5,79 @@ ENDM
 
 Landmarks: ; 0x1ca8c3
 	landmark   0,   0, SpecialMapName
-	landmark  44,  92, SunsetBayName
-	landmark  52, 100, SunsetCapeName
-	landmark  52,  88, Route1Name
-	landmark  52,  76, DaybreakVillageName
-	landmark  52,  64, Route2Name
-	landmark  64,  68, MtOnwaName
-	landmark  52,  52, GlintCityName
-	landmark  52,  44, GlintGroveName
-	landmark  76,  52, Route3Name
-	landmark  76,  44, StarglowCavernName
-	landmark  92,  68, StarglowValleyName
-	landmark 108,  68, Route4Name
-	landmark 108,  60, HuntersThicketName
-	landmark 100,  80, Route5Name
-	landmark  92,  92, Route6Name
-	landmark  84,  84, Route7Name
-	landmark  60,  84, LakeOnwaName
-	landmark  84, 116, SunbeamIslandName
-	landmark  92, 116, SunbeamJungleName
-	landmark  76,  68, Route8Name
-	landmark 116,  76, EventideForestName
-	landmark 124,  84, EventideVillageName
-	landmark 132,  84, Route9Name
-	landmark 124, 108, Route11Name
-	landmark 140,  76, DodrioRanchName
-	landmark 132,  60, FlickerStationName
-	landmark 132,  52, FlickerPassName
-	landmark 136,  44, Route10Name
+	landmark  44, 100, SunsetBayName
+	landmark  52, 108, SunsetCapeName
+	landmark  52,  96, Route1Name
+	landmark  52,  84, DaybreakVillageName
+	landmark  52,  72, Route2Name
+	landmark  60,  76, MtOnwaName
+	landmark  52,  60, GlintCityName
+	landmark  52,  52, GlintGroveName
+	landmark  76,  60, Route3Name
+	landmark  76,  52, StarglowCavernName
+	landmark  92,  76, StarglowValleyName
+	landmark 108,  76, Route4Name
+	landmark 108,  68, HuntersThicketName
+	landmark 100,  88, Route5Name
+	landmark  92, 100, Route6Name
+	landmark  84,  92, Route7Name
+	landmark  60,  92, LakeOnwaName
+	landmark  84, 124, SunbeamIslandName
+	landmark  92, 124, SunbeamJungleName
+	landmark  76,  76, Route8Name
+	landmark 116,  92, EventideForestName
+	landmark 124, 100, EventideVillageName
+	landmark 132,  92, Route9Name
+	landmark 124, 116, Route11Name
+	landmark 140,  92, DodrioRanchName
+	landmark 132,  76, FlickerStationName
+	landmark 132,  68, FlickerPassName
+	landmark 136,  48, Route10Name
 	landmark 132,  36, TwinkleTownName
 	landmark 140,  36, FrozenRuinsName
-	landmark  24,  48, LusterCityName
-	landmark  20,  44, LusterSewersName
-	landmark  20,  68, Route12Name
-	landmark  28,  68, SkateparkName
-	landmark  20,  84, AirportName
-	landmark  68, 100, Route13Name
-	landmark  44, 108, Route14Name
-	landmark  64, 116, Route15Name
-	landmark  84, 104, Route16Name
+	landmark  24,  40, LusterCityName
+	landmark  20,  36, LusterSewersName
+	landmark  20,  64, Route12Name
+	landmark  28,  60, SkateparkName
+	landmark  20,  76, AirportName
+	landmark  68, 108, Route13Name
+	landmark  44, 116, Route14Name
+	landmark  60, 124, Route15Name
+	landmark  84, 112, Route16Name
 	
-	landmark 120,  68, OldManorName
-	landmark  24,  48, LusterStationName
-	landmark  24,  48, ResidentialName
-	landmark  24,  48, ShoppingName
-	landmark  24,  48, BusinessName
-	landmark  52,  88, Route1_2Name
-	landmark  88,  44, StarglowDepthsName
+	landmark -16, -16, OldManorName
+	landmark  24,  40, LusterStationName
+	landmark  24,  40, ResidentialName
+	landmark  24,  40, ShoppingName
+	landmark  24,  40, BusinessName
+	landmark -16, -16, Route1_2Name
+	landmark  84,  44, StarglowDepthsName
 	landmark -16, -16, UnkownPassageName
 	landmark -16, -16, ConnectingCavernName
 	
 	landmark  36,  44, ShimmerName
 	landmark  44,  52, Route19Name
-	landmark  60,  52, SeasideCaveName
+	landmark  52,  44, SeasideCaveName
 	landmark  52,  76, Route20Name
 	landmark  60,  84, RadiantName
 	landmark  68,  76, Route21Name
 	landmark  68,  60, DuskTurnpikeName
-	landmark  80,  52, Route22Name
-	landmark 100,  52, CrossroadsName
-	landmark 100,  72, Route23Name
-	landmark 100,  92, KomoreVillageName
-	landmark  84,  84, Route24Name
-	landmark 116,  72, Route25Name
-	landmark 108,  88, Route26Name
-	landmark 116,  88, Route27Name
-	landmark 116, 100, BrightburgName
-	landmark 116, 112, Route28Name
-	landmark 108, 116, GreenGrottoName
-	landmark 104, 124, Route29Name
-	landmark  92, 124, ObscuraCityName
-	landmark 132,  96, Route30Name
-	landmark   0,   0, Route32Name
-	landmark 140,  68, AureoleTownName
+	landmark  88,  52, Route22Name
+	landmark 108,  52, CrossroadsName
+	landmark 108,  72, Route23Name
+	landmark 108,  92, KomoreVillageName
+	landmark  92,  84, Route24Name
+	landmark 124,  72, Route25Name
+	landmark 116,  88, Route26Name
+	landmark 124,  88, Route27Name
+	landmark 124, 100, BrightburgName
+	landmark 124, 112, Route28Name
+	landmark 116, 116, GreenGrottoName
+	landmark 104, 116, Route29Name
+	landmark  92, 116, ObscuraCityName
+	landmark 140,  96, Route30Name
+	landmark 108,  36, Route32Name
+	landmark -16, -16, AureoleTownName
 	
 	landmark  20, 124, BrilloTownName
 	landmark  32, 124, Route17Name

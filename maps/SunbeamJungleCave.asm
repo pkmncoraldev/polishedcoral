6ec401c2fe2d0775c7475160becaa15183be37ba
pkmncoraldev <pkmncoraldev@gmail.com>
2025-11-10 02:34:26 -0500

    Sunbeam Jungle Cave Redesign

--- maps/SunbeamJungleCave.asm
+++ maps/SunbeamJungleCave.asm
@@ -4,11 +4,12 @@ SunbeamJungleCave_MapScriptHeader:
 	db 0 ; callbacks
 
 	db 2 ; warp events
-	warp_def  9,  4, 3, SUNBEAM_JUNGLE
-	warp_def  5, 18, 1, SUNBEAM_JUNGLE_DEEP
+	warp_def 17,  4, 3, SUNBEAM_JUNGLE
+	warp_def  3, 16, 1, SUNBEAM_JUNGLE_DEEP
 
 	db 0 ; coord events
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 1 ; object events
+	itemball_event 10,  8, ULTRA_BALL, 1, EVENT_SUNBEAM_CAVE_ITEM

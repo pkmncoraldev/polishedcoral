8eded46ffec8fb5292fd199dd8e3584348b7f373
pkmncoraldev <pkmncoraldev@gmail.com>
2026-01-07 07:50:52 -0500

    Slightly Tweaked Lumina Town Music

--- audio/music/luminatown.asm
+++ audio/music/luminatown.asm
@@ -51,7 +51,7 @@ Music_LuminaTown_Ch1_loop:
 Music_LuminaTown_Ch1_Part_1:
 	dutycycle 1
 	vibrato $03, $23
-	notetype $c, $77
+	notetype $c, $67
 	octave 2
 	note B_, 2
 	note G#, 1
@@ -59,20 +59,20 @@ Music_LuminaTown_Ch1_Part_1:
 	note B_, 2
 	octave 3
 	note C#, 2
-	notetype $c, $68
+	notetype $c, $58
 	note D#, 4
-	notetype $c, $77
+	notetype $c, $67
 	octave 2
 	note B_, 2
 	octave 3
 	note D#, 2
-	notetype $c, $68
+	notetype $c, $58
 	note C#, 4
 	octave 2
 	note B_, 4
 	note A#, 4
 	note F#, 4
-	notetype $c, $77
+	notetype $c, $67
 	note B_, 2
 	note E_, 1
 	note F#, 1
@@ -85,43 +85,43 @@ Music_LuminaTown_Ch1_Part_1:
 	note G#, 1
 	note A#, 2
 	note F#, 2
-	notetype $c, $68
+	notetype $c, $58
 	note B_, 6
 	note A#, 1
 	note G#, 1
 	note G_, 4
 	note A#, 4
 	
-	notetype $c, $77
+	notetype $c, $67
 	note B_, 2
 	note G#, 1
 	note A#, 1
 	note B_, 2
 	octave 3
 	note C#, 2
-	notetype $c, $68
+	notetype $c, $58
 	note D#, 4
 	note G#, 4
 	note F#, 4
-	notetype $c, $77
+	notetype $c, $67
 	note E_, 2
 	note D#, 2
-	notetype $c, $68
+	notetype $c, $58
 	note C#, 4
-	notetype $c, $77
+	notetype $c, $67
 	note E_, 2
 	note F#, 2
-	notetype $c, $68
+	notetype $c, $58
 	note G#, 4
-	notetype $c, $77
+	notetype $c, $67
 	note F#, 2
 	note E_, 2
-	notetype $c, $68
+	notetype $c, $58
 	note D#, 4
 	note C#, 4
 	notetype $c, $3f
 	note C_, 4
-	notetype $c, $88
+	notetype $c, $78
 	note C_, 8
 	note __, 4
 	endchannel
@@ -130,37 +130,37 @@ Music_LuminaTown_Ch1_Part_1:
 Music_LuminaTown_Ch1_Part_2:
 	dutycycle 1
 	octave 3
-	notetype $c, $77
+	notetype $c, $67
 	note G#, 2
 	note E_, 1
 	note F#, 1
 	note G#, 2
 	note E_, 2
-	notetype $c, $68
+	notetype $c, $58
 	note B_, 4
 	note G#, 4
 	note F#, 6
 	note E_, 2
 	note D#, 8
-	notetype $c, $77
+	notetype $c, $67
 	note E_, 2
 	note C#, 1
 	note D#, 1
 	note E_, 2
 	note C#, 2
-	notetype $c, $68
+	notetype $c, $58
 	note G#, 4
 	note E_, 4
 	note G_, 8
 	note G_, 4
 	note A#, 4
-	notetype $c, $77
+	notetype $c, $67
 	note G#, 2
 	note E_, 1
 	note F#, 1
 	note G#, 2
 	note E_, 2
-	notetype $c, $68
+	notetype $c, $58
 	note B_, 4
 	note G#, 4
 	note F#, 6
@@ -177,9 +177,9 @@ Music_LuminaTown_Ch1_Part_2:
 	octave 3
 	note A#, 2
 	note B_, 14
-	notetype $c, $67
-	note B_, 2
 	notetype $c, $57
+	note B_, 2
+	notetype $c, $47
 	octave 2
 	note B_, 6
 	note __, 2

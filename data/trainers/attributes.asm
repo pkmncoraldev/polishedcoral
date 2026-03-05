5cf6cddd6abb9a7ac7c931cb9b2303f380356330
pkmncoraldev <pkmncoraldev@gmail.com>
2026-03-04 23:56:49 -0500

    More Trainer Balance Tweaks

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -339,7 +339,7 @@ TrainerClassAttributes: ; 3959c
 ; Teacher
 	db 0, 0 ; items
 	db 18 ; base reward
-	dw AI_BASIC + AI_OPPORTUNIST + AI_AGGRESSIVE + AI_STATUS
+	dw AI_BASIC + AI_TYPES + AI_OPPORTUNIST + AI_AGGRESSIVE + AI_STATUS
 	dw CONTEXT_USE + SWITCH_SOMETIMES
 
 ; Guitarist

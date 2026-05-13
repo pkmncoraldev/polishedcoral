61315cf9e377d053d93f7fe95ddbdcb14a20de2f
pkmncoraldev <pkmncoraldev@gmail.com>
2026-05-13 02:43:32 -0400

    Improved Dragon Shrine Scene Music... AGAIN

--- audio/music/encounter/rival_encounter2.asm
+++ audio/music/encounter/rival_encounter2.asm
@@ -6,12 +6,88 @@ Music_RivalEncounter2:
 Music_RivalEncounter2_Ch1:
 	tempo 210
 	volume $77
+	dutycycle $1
+	vibrato $8, $36
+	stereopanning $ff
+	notetype $c, $97
+	note __, 2
+Music_RivalEncounter2_Ch1_Loop:
+	octave 1
+	note C#, 1
+	note __, 1
+	note C#, 1
+	note __, 5
+	note G#, 6
+	octave 2
+	note C_, 2
+	note C#, 8
+	octave 1
+	note G#, 6
+	note E_, 2
+	note C#, 1
+	note __, 1
+	note C#, 1
+	
+	dutycycle $0
+	notetype $c, $1e
+	octave 3
+	note E_, 1
+	notetype $c, $38
+	note E_, 4
+	note G#, 4
+	note G#, 4
+	note F#, 4
+	note F#, 4
+	note D#, 4
+	note D#, 2
+	dutycycle $1
+	notetype $c, $97
+	octave 1
+	note E_, 2
+	
+	note C#, 1
+	note __, 1
+	note C#, 1
+	note __, 5
+	note G#, 6
+	octave 2
+	note C_, 2
+	note C#, 8
+	octave 1
+	note G#, 6
+	note E_, 2
+	note C#, 1
+	note __, 1
+	note C#, 1
+	
+	dutycycle $2
+	notetype $c, $58
+	octave 3
+	note E_, 1
+	note G#, 1
+	note __, 1
+	note G#, 4
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note F#, 4
+	note D#, 4
+	note C_, 4
+	note D#, 2
+	dutycycle $1
+	notetype $c, $97
+	octave 1
+	note E_, 2
+	loopchannel 0, Music_RivalEncounter2_Ch1_Loop
+	endchannel
+
+Music_RivalEncounter2_Ch2:
 	tone $0001
 	vibrato $12, $15
 	stereopanning $0f
-	notetype $c, $58
+	notetype $c, $48
 	note __, 2
-Music_RivalEncounter2_Ch1_Loop:
+Music_RivalEncounter2_Ch2_Loop:
 	dutycycle $2
 	octave 3
 	note C#, 3
@@ -35,85 +111,13 @@ Music_RivalEncounter2_Ch1_Loop:
 	note C_, 8
 	octave 2
 	note G#, 8
-	loopchannel 0, Music_RivalEncounter2_Ch1_Loop
-	endchannel
-
-Music_RivalEncounter2_Ch2:
-	dutycycle $1
-	vibrato $8, $36
-	stereopanning $ff
-	notetype $c, $77
-	note __, 2
-Music_RivalEncounter2_Ch2_Loop:
-	octave 1
-	note C#, 1
-	note __, 1
-	note C#, 1
-	note __, 5
-	note G#, 6
-	octave 2
-	note C_, 2
-	note C#, 8
-	octave 1
-	note G#, 6
-	note E_, 2
-	note C#, 1
-	note __, 1
-	note C#, 1
-	
-	dutycycle $0
-	notetype $c, $38
-	octave 3
-	note E_, 5
-	note G#, 8
-	note F#, 8
-	note D#, 6
-	dutycycle $1
-	notetype $c, $77
-	octave 1
-	note E_, 2
-	
-	note C#, 1
-	note __, 1
-	note C#, 1
-	note __, 5
-	note G#, 6
-	octave 2
-	note C_, 2
-	note C#, 8
-	octave 1
-	note G#, 6
-	note E_, 2
-	note C#, 1
-	note __, 1
-	note C#, 1
-	
-	dutycycle $2
-	notetype $c, $48
-	octave 3
-	note E_, 1
-	note G#, 1
-	note __, 1
-	note G#, 4
-	note E_, 2
-	note G#, 2
-	note E_, 2
-	note F#, 4
-	note D#, 4
-	note C_, 4
-	note D#, 2
-	dutycycle $1
-	notetype $c, $77
-	octave 1
-	note E_, 2
-;	endchannel
 	loopchannel 0, Music_RivalEncounter2_Ch2_Loop
 	endchannel
 
 Music_RivalEncounter2_Ch3:
 	stereopanning $f0
 Music_RivalEncounter2_Ch3_Loop:
-	notetype $c, $20
+	notetype $c, $10
 	octave 4
 	note C#, 1
 	note D#, 1
@@ -126,7 +130,7 @@ Music_RivalEncounter2_Ch3_Loop:
 	note F#, 2
 	note D#, 2
 	note E_, 2
-	notetype $c, $31
+	notetype $c, $21
 	octave 5
 	note C#, 8
 	note E_, 8

2418307b0862a011eb34e3508f1de2c1c41d6d10
pkmncoraldev <pkmncoraldev@gmail.com>
2026-08-01 17:02:42 -0400

    Aureole Mountain Trainer Text

--- maps/AureoleMountain1F.asm
+++ maps/AureoleMountain1F.asm
@@ -18,10 +18,9 @@ AureoleMountain1F_MapScriptHeader:
 
 	db 0 ; coord events
 
-	db 3 ; bg events
+	db 2 ; bg events
 	bg_event 25, 37, SIGNPOST_ITEM + HYPER_POTION, EVENT_AUREOLE_MOUNTAIN_1F_HIDDEN_ITEM_1
 	bg_event 15, 29, SIGNPOST_ITEM + PP_UP, EVENT_AUREOLE_MOUNTAIN_1F_HIDDEN_ITEM_2
-	bg_event  3, 31, SIGNPOST_ITEM + SUPER_REPEL, EVENT_AUREOLE_MOUNTAIN_1F_HIDDEN_ITEM_3
 
 	db 11 ; object events
 	person_event SPRITE_SUPER_NERD, 38,  8, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, (1 << 3) | PAL_OW_BROWN, PERSONTYPE_GENERICTRAINER, 3, AureoleMountain1F_Trainer_1, -1
@@ -30,7 +29,7 @@ AureoleMountain1F_MapScriptHeader:
 	person_event SPRITE_DELINQUENT_M, 24, 11, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_GENERICTRAINER, 4, AureoleMountain1F_Trainer_4, -1
 	person_event SPRITE_COOLTRAINER_M, 34, 11, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_GENERICTRAINER, 2, AureoleMountain1F_Trainer_5, -1
 	person_event SPRITE_COOLTRAINER_F, 31, 24, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_GENERICTRAINER, 3, AureoleMountain1F_Trainer_6, -1
-	tapeball_event 33, 41, MUSIC_TRAINER_VICTORY, 2, MUSIC_TRAINER_VICTORY
+	tapeball_event 33, 41, MUSIC_TRAINER_VICTORY, 2, EVENT_MUSIC_TRAINER_VICTORY
 	itemball_event 11, 32, EXPERT_BELT, 1, EVENT_AUREOLE_MOUNTAIN_1F_ITEM
 	strengthboulder_event 18, 44, -1
 
@@ -42,83 +41,132 @@ AureoleMountain1FCallback:
 AureoleMountain1F_Trainer_1:
 	generictrainer SUPER_NERD, ADAM, EVENT_BEAT_AUREOLE_MOUNTAIN_1F_TRAINER_1, .SeenText, .BeatenText
 
-	text "NORMAL TEXT"
+	text "I might be in"
+	line "over my head here…"
 	done
 
 .SeenText:
-	text "SEEN TEXT"	
+	text "Hehe…"
+	
+	para "You think you're"
+	line "ready to climb up"
+	cont "AUREOLE MOUNTAIN?"
+	
+	para "Let's find out!"
 	done
 
 .BeatenText:
-	text "YOU WIN"
+	text "More ready than"
+	line "me!"
 	done
 	
 AureoleMountain1F_Trainer_2:
 	generictrainer POKEMANIAC, BENNY, EVENT_BEAT_AUREOLE_MOUNTAIN_1F_TRAINER_2, .SeenText, .BeatenText
 
-	text "NORMAL TEXT"
+	text "I won't let this"
+	line "minor setback stop"
+	cont "me!"
 	done
 
 .SeenText:
-	text "SEEN TEXT"	
+	text "You climbing the"
+	line "mountain too?"
+	
+	para "I bet their are"
+	line "some crazy rare"
+	cont "#MON up top!"
 	done
 
 .BeatenText:
-	text "YOU WIN"
+	text "Graaah!"
 	done
 	
 AureoleMountain1F_Trainer_3:
 	generictrainer POKEMANIAC, MILO, EVENT_BEAT_AUREOLE_MOUNTAIN_1F_TRAINER_3, .SeenText, .BeatenText
 
-	text "NORMAL TEXT"
+	text "Tell me all about"
+	line "the #MON you've"
+	cont "seen on your"
+	cont "travels!"
 	done
 
 .SeenText:
-	text "SEEN TEXT"	
+	text "You look fairly"
+	line "tough."
+	
+	para "You must have seen"
+	line "all kinds of #-"
+	cont "MON on your"
+	cont "travels!"
+	
+	para "Tell me about"
+	line "them all!"
 	done
 
 .BeatenText:
-	text "YOU WIN"
+	text "Wow! So tough!"
 	done
 	
 AureoleMountain1F_Trainer_4:
 	generictrainer DELINQUENT_M, QUINCY, EVENT_BEAT_AUREOLE_MOUNTAIN_1F_TRAINER_4, .SeenText, .BeatenText
 
-	text "NORMAL TEXT"
+	text "You knocked my"
+	line "my #MON out"
+	cont "like a light!"
 	done
 
 .SeenText:
-	text "SEEN TEXT"	
+	text "Hah!"
+	
+	para "Little kiddy lost"
+	line "in the mountains?"
+	
+	para "We'll put'cha to"
+	line "bed!"
 	done
 
 .BeatenText:
-	text "YOU WIN"
+	text "Ayee!"
 	done
 	
 AureoleMountain1F_Trainer_5:
 	generictrainer COOLTRAINERM, LUCAS, EVENT_BEAT_AUREOLE_MOUNTAIN_1F_TRAINER_5, .SeenText, .BeatenText
-
-	text "NORMAL TEXT"
+	
+	text "Good luck!"
+	
+	para "Maybe I'll see"
+	line "you up there."
 	done
 
 .SeenText:
-	text "SEEN TEXT"	
+	text "Feeling tired yet?"
+	
+	para "I hope not,"
+	line "because you're just"
+	cont "getting started!"
 	done
 
 .BeatenText:
-	text "YOU WIN"
+	text "Doing fine!"
 	done
 	
 AureoleMountain1F_Trainer_6:
 	generictrainer COOLTRAINERF, BROOKE, EVENT_BEAT_AUREOLE_MOUNTAIN_1F_TRAINER_6, .SeenText, .BeatenText
 
-	text "NORMAL TEXT"
+	text "I'm just gonna rest"
+	line "here for a while…"
 	done
 
 .SeenText:
-	text "SEEN TEXT"	
+	text "Ow ow ow!"
+	
+	para "I think I sprained"
+	line "my ankle falling"
+	cont "down a hole!"
+	
+	para "Be careful!"
 	done
 
 .BeatenText:
-	text "YOU WIN"
+	text "Yeowch!"
 	done
--- maps/AureoleMountain2F.asm
+++ maps/AureoleMountain2F.asm
@@ -12,7 +12,8 @@ AureoleMountain2F_MapScriptHeader:
 
 	db 0 ; coord events
 
-	db 0 ; bg events
+	db 1 ; bg events
+	bg_event 17, 18, SIGNPOST_ITEM + SUPER_REPEL, EVENT_AUREOLE_MOUNTAIN_2F_HIDDEN_ITEM_1
 
 	db 6 ; object events
 	object_event 14,  7, SPRITE_DISGUISEMAN, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_SCRIPT, 0, ObjectEvent, EVENT_AUREOLE_MOUNTAIN_2F_DISGUISEMAN_BLUE
@@ -20,7 +21,7 @@ AureoleMountain2F_MapScriptHeader:
 	person_event SPRITE_POKEMANIAC, 12,  5, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_GENERICTRAINER, 3, AureoleMountain2F_Trainer_1, -1
 	person_event SPRITE_COOLTRAINER_F,  2, 25, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_GENERICTRAINER, 3, AureoleMountain2F_Trainer_2, -1
 	person_event SPRITE_HIKER, 19, 26, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, (1 << 3) | PAL_OW_PURPLE, PERSONTYPE_GENERICTRAINER, 3, AureoleMountain2F_Trainer_3, -1
-	person_event SPRITE_POKEMANIAC, 10, 21, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_GENERICTRAINER, 2, AureoleMountain2F_Trainer_4, -1
+	person_event SPRITE_POKEMANIAC, 10, 20, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_GENERICTRAINER, 2, AureoleMountain2F_Trainer_4, -1
 
 	
 AureoleMountain2FTrigger0:
@@ -38,57 +39,98 @@ AureoleMountain2FCoordAsm:
 AureoleMountain2F_Trainer_1:
 	generictrainer PSYCHIC_T, JACQUES, EVENT_BEAT_AUREOLE_MOUNTAIN_2F_TRAINER_1, .SeenText, .BeatenText
 
-	text "NORMAL TEXT"
+	text "I can't believe it!"
+	
+	para "My visions have"
+	line "never been wrong!"
+	
+	para "Actually, they"
+	line "have…"
+	
+	para "Hehe…"
 	done
 
 .SeenText:
-	text "SEEN TEXT"	
+	text "I see…"
+	
+	para "I see…"
+	
+	para "An easy victory!"
 	done
 
 .BeatenText:
-	text "YOU WIN"
+	text "But my vision!"
 	done
 	
 AureoleMountain2F_Trainer_2:
 	generictrainer COOLTRAINERF, MAE, EVENT_BEAT_AUREOLE_MOUNTAIN_2F_TRAINER_2, .SeenText, .BeatenText
 
-	text "NORMAL TEXT"
+	text "Alright."
+	
+	para "You might be just"
+	line "a bit better than"
+	cont "me…"
 	done
 
 .SeenText:
-	text "SEEN TEXT"	
+	text "How tough are you,"
+	line "huh?"
+	
+	para "Let's find out!"
 	done
 
 .BeatenText:
-	text "YOU WIN"
+	text "I coulda had"
+	line "that!"
 	done
 	
 AureoleMountain2F_Trainer_3:
 	generictrainer HIKER, BRUCE, EVENT_BEAT_AUREOLE_MOUNTAIN_2F_TRAINER_3, .SeenText, .BeatenText
 
-	text "NORMAL TEXT"
+	text "I made it to the"
+	line "ladder up ahead,"
+	
+	para "but I couldn't"
+	line "find my way any"
+	cont "further."
+	
+	para "It's so confusing"
+	line "up there!"
 	done
 
 .SeenText:
-	text "SEEN TEXT"	
+	text "This might be"
+	line "where I have to"
+	cont "turn back."
+	
+	para "Let's have a battle"
+	line "while I give it"
+	cont "some thought."
 	done
 
 .BeatenText:
-	text "YOU WIN"
+	text "Guess it's over"
+	line "for me."
 	done
 	
 AureoleMountain2F_Trainer_4:
 	generictrainer PSYCHIC_T, RHETT, EVENT_BEAT_AUREOLE_MOUNTAIN_2F_TRAINER_4, .SeenText, .BeatenText
 
-	text "NORMAL TEXT"
+	text "You really should"
+	line "watch where you're"
+	cont "falling!"
 	done
 
 .SeenText:
-	text "SEEN TEXT"	
+	text "Woah!"
+	
+	para "Where the heck"
+	line "did you come from?"
 	done
 
 .BeatenText:
-	text "YOU WIN"
+	text "I didn't see"
+	line "that coming!"
 	done
 	
 AureoleMountain2FDisguiseman:
